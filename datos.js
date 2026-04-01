574601f1955f81100646a00421c21678c093626c
resettalleraz-max <resettalleraz@gmail.com>
2026-03-31 18:21:59 -0600

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,1 +1,19 @@
+const PANTALLAS = [
+    ["TITULO","--- IPHONE ---"],
+    ["iPhone 6G","₡10,000"],
+    ["iPhone 7G","₡12,000"],
+    ["iPhone 8G","₡12,000"],
+    ["iPhone X Incell","₡20,000"],
+    
+    ["TITULO","--- HONOR ---"],
+    ["Honor 50 Lite","₡17,000"],
+    ["<span style='color:#eab308'>Honor X9A ORIGINAL</span>","₡33,000"],
+    
+    ["TITULO","--- SAMSUNG ---"],
+    ["Samsung A10","₡11,000"],
+    ["Samsung A12","₡15,000"],
+    ["<span style='color:#eab308'>Samsung A54 ORIGINAL (Marco)</span>","₡57,000"]
+];
 
+const FLEX = [];
+const HERRAMIENTAS = [];
