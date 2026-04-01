c6a26fed28e5805d9e39cff023ae0c15505294e8
resettalleraz-max <resettalleraz@gmail.com>
2026-03-31 22:25:58 -0600

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,27 +1,37 @@
 const PANTALLAS = [
-    ["TITULO","--- IPHONE (INSTALADA) ---"],
-    ["iPhone 6G","₡22,000"], ["iPhone 6S","₡22,000"], ["iPhone 6 Plus","₡25,000"], ["iPhone 6S Plus","₡25,000"], ["iPhone 7G","₡25,000"], ["iPhone 8G","₡25,000"], ["iPhone 7 Plus","₡28,000"], ["iPhone 8 Plus","₡28,000"], ["iPhone X Incell","₡35,000"], ["iPhone XS Incell","₡35,000"], ["iPhone XR","₡35,000"], ["iPhone 11 Incell","₡35,000"], ["iPhone 11 Pro Incell","₡38,000"], ["iPhone 11 Pro Max Incell","₡43,000"], ["iPhone 12 / 12 Pro","₡45,000"], ["iPhone 12 Pro Max","₡50,000"], ["iPhone 13","₡50,000"], ["iPhone 14","₡55,000"], ["iPhone 15","₡65,000"], ["iPhone 16 Pro Max","₡85,000"],
-    
-    ["TITULO","--- IPHONE OLED / ORIGINAL ---"],
-    ["<span style='color:#eab308'>iPhone X OLED</span>","₡45,000"], ["<span style='color:#eab308'>iPhone XS OLED</span>","₡45,000"], ["<span style='color:#eab308'>iPhone XS Max OLED</span>","₡52,000"], ["<span style='color:#eab308'>iPhone 11 ORIGINAL</span>","₡48,000"], ["<span style='color:#eab308'>iPhone 11 Pro Max OLED</span>","₡55,000"], ["<span style='color:#eab308'>iPhone 12 / 12 Pro OLED</span>","₡60,000"], ["<span style='color:#eab308'>iPhone 13 OLED Soft</span>","₡70,000"], ["<span style='color:#eab308'>iPhone 13 Pro ORIGINAL</span>","₡135,000"], ["<span style='color:#eab308'>iPhone 14 Pro ORIGINAL</span>","₡125,000"], ["<span style='color:#eab308'>iPhone 15 Pro Max ORIGINAL</span>","₡145,000"],
+    ["TITULO", "IPHONE"],
+    ["SUBTITULO", "Pantallas con Marco"],
+    ["iPhone 6G / 6S", "₡22,000"], ["iPhone 7G / 8G", "₡25,000"],
+    ["SUBTITULO", "Pantallas sin Marco"],
+    ["iPhone XR / 11", "₡35,000"],
+    ["<span style='color:#eab308; font-weight:bold;'>[ORIGINAL]</span> iPhone 11", "₡48,000"],
+    ["<span style='color:#eab308; font-weight:bold;'>[OLED]</span> iPhone X / XS", "₡45,000"],
 
-    ["TITULO","--- SAMSUNG (CON MARCO) ---"],
-    ["Samsung J2 Prime","₡20,000"], ["Samsung J4 Plus","₡25,000"], ["Samsung J6 Plus","₡25,000"], ["Samsung J7 Prime","₡22,000"], ["Samsung A10","₡25,000"], ["Samsung A11","₡25,000"], ["Samsung A12","₡25,000"], ["Samsung A13","₡30,000"], ["Samsung A14","₡30,000"], ["Samsung A15","₡30,000"], ["Samsung A20","₡30,000"], ["Samsung A21S","₡30,000"], ["Samsung A22","₡30,000"], ["Samsung A30","₡32,000"], ["Samsung A31","₡32,000"], ["Samsung A32","₡32,000"], ["Samsung A50","₡35,000"], ["Samsung A51","₡35,000"], ["Samsung A52","₡35,000"], ["Samsung A70","₡38,000"], ["Samsung A71","₡38,000"], ["<span style='color:#eab308'>Samsung A54 ORIGINAL</span>","₡75,000"], ["<span style='color:#eab308'>Samsung A55 ORIGINAL</span>","₡80,000"],
+    ["TITULO", "SAMSUNG"],
+    ["SUBTITULO", "Pantallas con Marco"],
+    ["Samsung A10", "₡25,000"], ["Samsung A12", "₡25,000"],
+    ["<span style='color:#eab308; font-weight:bold;'>[ORIGINAL]</span> Samsung A54", "₡75,000"],
+    ["SUBTITULO", "Pantallas sin Marco (Sencilla)"],
+    ["Samsung A10S", "₡25,000"], ["Samsung A03", "₡25,000"],
 
-    ["TITULO","--- SAMSUNG (SENCILLA / SIN MARCO) ---"],
-    ["Samsung A01","₡25,000"], ["Samsung A02","₡25,000"], ["Samsung A03","₡25,000"], ["Samsung A04","₡28,000"], ["Samsung A04S","₡28,000"], ["Samsung A05","₡28,000"], ["Samsung A10S","₡25,000"], ["Samsung A23","₡32,000"], ["Samsung A24","₡32,000"], ["Samsung A25","₡32,000"], ["Samsung A33","₡35,000"], ["Samsung A34","₡35,000"], ["Samsung A35","₡35,000"], ["Samsung A53","₡40,000"], ["Samsung A54","₡40,000"], ["Samsung A55","₡40,000"],
+    ["TITULO", "HUAWEI"],
+    ["SUBTITULO", "Pantallas con Marco"],
+    ["Huawei Y9 Prime", "₡30,000"], ["Huawei Nova 5T", "₡32,000"],
+    ["SUBTITULO", "Pantallas sin Marco"],
+    ["Huawei Y9 2019", "₡28,000"], ["Huawei P30 Lite", "₡30,000"],
+    ["<span style='color:#eab308; font-weight:bold;'>[ORIGINAL]</span> Huawei Nova 9", "₡55,000"],
 
-    ["TITULO","--- SAMSUNG GAMA ALTA (S / NOTE) ---"],
-    ["<span style='color:#eab308'>Samsung S20 FE ORIGINAL</span>","₡65,000"], ["<span style='color:#eab308'>Samsung S21 ORIGINAL</span>","₡85,000"], ["<span style='color:#eab308'>Samsung S21 FE ORIGINAL</span>","₡85,000"], ["<span style='color:#eab308'>Samsung S22 ORIGINAL</span>","₡95,000"], ["<span style='color:#eab308'>Samsung S22 Ultra ORIG</span>","₡165,000"], ["<span style='color:#eab308'>Samsung S23 Ultra ORIG</span>","₡185,000"], ["<span style='color:#eab308'>Samsung S24 Ultra ORIG</span>","₡195,000"], ["<span style='color:#eab308'>Samsung Note 10+ ORIG</span>","₡145,000"], ["<span style='color:#eab308'>Samsung Note 20 Ultra ORIG</span>","₡165,000"],
+    ["TITULO", "HONOR"],
+    ["SUBTITULO", "Pantallas con Marco"],
+    ["Honor X6", "₡28,000"], ["Honor X7", "₡30,000"],
+    ["SUBTITULO", "Pantallas sin Marco"],
+    ["Honor X7B", "₡35,000"], ["<span style='color:#eab308; font-weight:bold;'>[ORIGINAL]</span> Honor X9A", "₡55,000"],
 
-    ["TITULO","--- HONOR / HUAWEI (CON MARCO) ---"],
-    ["Honor X6","₡28,000"], ["Honor X7","₡30,000"], ["Honor X8","₡32,000"], ["Honor X8B","₡35,000"], ["Huawei Y9 Prime","₡30,000"], ["Huawei Y9S","₡30,000"], ["Huawei Nova 5T","₡32,000"],
-
-    ["TITULO","--- HONOR / HUAWEI (SIN MARCO) ---"],
-    ["Honor X6S","₡28,000"], ["Honor X7A","₡30,000"], ["Honor X7B","₡35,000"], ["Honor X8A","₡32,000"], ["<span style='color:#eab308'>Honor X9A ORIGINAL</span>","₡55,000"], ["Honor 90 Lite","₡30,000"], ["Huawei Y7 2019","₡28,000"], ["Huawei Y9 2019","₡28,000"], ["Huawei P30 Lite","₡30,000"], ["Huawei P40 Lite","₡30,000"], ["<span style='color:#eab308'>Huawei Nova 9 OLED</span>","₡55,000"],
-
-    ["TITULO","--- XIAOMI / MOTOROLA (INSTALADA) ---"],
-    ["Redmi 9","₡28,000"], ["Redmi 10","₡28,000"], ["Redmi 12","₡28,000"], ["Redmi 13C","₡28,000"], ["Redmi Note 11","₡32,000"], ["Redmi Note 12","₡32,000"], ["Redmi Note 13 4G","₡35,000"], ["Redmi A1 / A2 / A3","₡25,000"], ["Poco X3 Pro","₡35,000"], ["<span style='color:#eab308'>Poco F3 / F4 OLED</span>","₡55,000"], ["Moto G22","₡28,000"], ["Moto G24","₡28,000"], ["Moto G54","₡32,000"], ["Moto G84","₡32,000"], ["Moto E13 / E20","₡25,000"], ["Moto Edge 40 Neo","₡65,000"]
+    ["TITULO", "XIAOMI / MOTOROLA"],
+    ["SUBTITULO", "Pantallas con Marco"],
+    ["Moto G24", "₡28,000"], ["Moto G84", "₡32,000"],
+    ["SUBTITULO", "Pantallas sin Marco"],
+    ["Redmi Note 12", "₡32,000"], ["Redmi 13C", "₡28,000"]
 ];
 
 const FLEX = [];
