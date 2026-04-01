2aa9863085b6b5486592b28a2c50800566a3d798
resettalleraz-max <resettalleraz@gmail.com>
2026-03-31 21:37:04 -0600

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,15 +1,21 @@
 const PANTALLAS = [
     ["TITULO","--- IPHONE (INSTALADA) ---"],
-    ["iPhone 6G / 6S","₡22,000"], ["iPhone 7G / 8G","₡25,000"], ["iPhone 7 Plus / 8 Plus","₡28,000"], ["iPhone X Incell","₡35,000"], ["iPhone XR / 11 Incell","₡35,000"], ["iPhone 12 / 12 Pro Incell","₡45,000"], ["iPhone 13","₡50,000"], ["iPhone 14","₡55,000"], ["iPhone 15","₡65,000"], ["iPhone 16 Pro Max","₡85,000"],
-
-    ["TITULO","--- IPHONE OLED/ORIG (INSTALADA) ---"],
-    ["<span style='color:#eab308'>iPhone X / XS OLED</span>","₡45,000"], ["<span style='color:#eab308'>iPhone 11 ORIGINAL</span>","₡48,000"], ["<span style='color:#eab308'>iPhone 12 / 12 Pro OLED</span>","₡60,000"], ["<span style='color:#eab308'>iPhone 13 Pro ORIGINAL</span>","₡135,000"], ["<span style='color:#eab308'>iPhone 15 Pro Max ORIGINAL</span>","₡145,000"],
-
-    ["TITULO","--- HONOR / HUAWEI (INSTALADA) ---"],
-    ["Honor X6 / X7 / X8","₡28,000"], ["<span style='color:#eab308'>Honor X9A ORIGINAL</span>","₡55,000"], ["Honor 90 Lite","₡30,000"], ["Huawei Y9 Prime","₡30,000"], ["<span style='color:#eab308'>Huawei Nova 9 OLED</span>","₡55,000"],
+    ["iPhone 6G / 6S","₡22,000"], ["iPhone 6 Plus / 6S Plus","₡25,000"], ["iPhone 7G / 8G","₡25,000"], ["iPhone 7 Plus / 8 Plus","₡28,000"], ["iPhone X Incell","₡35,000"], ["iPhone XS Incell","₡35,000"], ["iPhone XR","₡35,000"], ["iPhone 11 Incell","₡35,000"], ["iPhone 11 Pro Incell","₡38,000"], ["iPhone 11 Pro Max Incell","₡43,000"], ["iPhone 12 / 12 Pro Incell","₡45,000"], ["iPhone 12 Pro Max","₡50,000"], ["iPhone 13","₡50,000"], ["iPhone 14","₡55,000"], ["iPhone 15","₡65,000"], ["iPhone 16 Pro Max","₡85,000"],
+    
+    ["TITULO","--- IPHONE OLED / ORIGINAL (INSTALADA) ---"],
+    ["<span style='color:#eab308'>iPhone X / XS OLED</span>","₡45,000"], ["<span style='color:#eab308'>iPhone 11 ORIGINAL</span>","₡48,000"], ["<span style='color:#eab308'>iPhone 12 / 12 Pro OLED</span>","₡60,000"], ["<span style='color:#eab308'>iPhone 13 OLED Soft</span>","₡70,000"], ["<span style='color:#eab308'>iPhone 13 Pro ORIGINAL</span>","₡135,000"], ["<span style='color:#eab308'>iPhone 15 Pro Max ORIGINAL</span>","₡145,000"],
 
     ["TITULO","--- SAMSUNG SERIE A / J (INSTALADA) ---"],
-    ["Samsung J2 Prime","₡20,000"], ["Samsung J7 Prime","₡22,000"], ["Samsung A10 / A11","₡25,000"], ["Samsung A12 / A13","₡28,000"], ["Samsung A14 / A15","₡30,000"], ["Samsung A20 / A21S / A22","₡30,000"], ["Samsung A30 / A31 / A32","₡32,000"], ["Samsung A50 / A51 / A52","₡35,000"], ["<span style='color:#eab308'>Samsung A54 ORIGINAL</span>","₡75,000"], ["<span style='color:#eab308'>Samsung A55 ORIGINAL</span>","₡80,000"]
+    ["Samsung J2 Prime","₡20,000"], ["Samsung J7 Prime","₡22,000"], ["Samsung A10 / A11","₡25,000"], ["Samsung A12 / A13","₡28,000"], ["Samsung A14 / A15","₡30,000"], ["Samsung A20 / A21S / A22","₡30,000"], ["Samsung A30 / A31 / A32","₡32,000"], ["Samsung A50 / A51 / A52","₡35,000"], ["Samsung A70 / A71","₡38,000"], ["<span style='color:#eab308'>Samsung A54 ORIGINAL</span>","₡75,000"], ["<span style='color:#eab308'>Samsung A55 ORIGINAL</span>","₡80,000"],
+
+    ["TITULO","--- SAMSUNG GAMA ALTA (S / NOTE) ---"],
+    ["<span style='color:#eab308'>Samsung S20 FE ORIGINAL</span>","₡65,000"], ["<span style='color:#eab308'>Samsung S21 ORIGINAL</span>","₡85,000"], ["<span style='color:#eab308'>Samsung S22 Ultra ORIG</span>","₡165,000"], ["<span style='color:#eab308'>Samsung Note 10 Plus ORIG</span>","₡145,000"], ["<span style='color:#eab308'>Samsung S23 Ultra ORIG</span>","₡185,000"],
+
+    ["TITULO","--- HONOR / HUAWEI (INSTALADA) ---"],
+    ["Honor X6 / X7 / X8","₡28,000"], ["Honor X7B / X8B","₡35,000"], ["<span style='color:#eab308'>Honor X9A ORIGINAL</span>","₡55,000"], ["Honor 90 Lite","₡30,000"], ["Huawei Y9 Prime","₡30,000"], ["Huawei P30 Lite","₡28,000"], ["<span style='color:#eab308'>Huawei Nova 9 OLED</span>","₡55,000"],
+
+    ["TITULO","--- XIAOMI / MOTOROLA (INSTALADA) ---"],
+    ["Redmi Note 10 / 11 / 12","₡32,000"], ["Redmi Note 13 4G","₡35,000"], ["Redmi 10 / 12 / 13C","₡28,000"], ["<span style='color:#eab308'>Poco F3 / F4 / F5 OLED</span>","₡55,000"], ["Moto G22 / G23 / G24","₡28,000"], ["Moto G50 / G54 / G84","₡32,000"], ["Moto Edge 30 / 40 Neo","₡65,000"]
 ];
 
 const FLEX = [];
