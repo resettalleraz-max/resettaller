4133ca8c45fc6a9e8488881c110932bd890cd4b8
resettalleraz-max <resettalleraz@gmail.com>
2026-03-31 20:49:01 -0600

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,12 +1,15 @@
 const PANTALLAS = [
     ["TITULO","--- IPHONE (PANTALLA INSTALADA) ---"],
-    ["iPhone 6G / 6S","₡22,000"], ["iPhone 7G / 8G","₡25,000"], ["iPhone 7 Plus / 8 Plus","₡28,000"], ["iPhone X Incell","₡35,000"], ["iPhone XR / 11 Incell","₡35,000"], ["iPhone 11 Pro Incell","₡38,000"], ["iPhone 12 / 12 Pro Incell","₡45,000"], ["iPhone 13 Incell","₡50,000"], ["iPhone 14 Incell","₡55,000"], ["iPhone 15 Incell","₡65,000"], ["iPhone 16 Pro Max Incell","₡85,000"],
+    ["iPhone 6G / 6S","₡22,000"], ["iPhone 6G Plus / 6S Plus","₡25,000"], ["iPhone 7G / 8G","₡25,000"], ["iPhone 7 Plus / 8 Plus","₡28,000"], ["iPhone X Incell","₡35,000"], ["iPhone XS Incell","₡35,000"], ["iPhone XR","₡35,000"], ["iPhone 11 Incell","₡35,000"], ["iPhone 11 Pro Incell","₡38,000"], ["iPhone 11 Pro Max Incell","₡43,000"], ["iPhone 12 / 12 Pro Incell","₡45,000"], ["iPhone 12 Pro Max Incell","₡50,000"], ["iPhone 13 Incell","₡50,000"], ["iPhone 14 Incell","₡55,000"], ["iPhone 15 Incell","₡65,000"], ["iPhone 16 Pro Max Incell","₡85,000"],
     
     ["TITULO","--- IPHONE OLED / ORIGINAL (INSTALADA) ---"],
-    ["<span style='color:#eab308'>iPhone X / XS OLED</span>","₡45,000"], ["<span style='color:#eab308'>iPhone 11 ORIGINAL</span>","₡48,000"], ["<span style='color:#eab308'>iPhone 12 / 12 Pro OLED</span>","₡60,000"], ["<span style='color:#eab308'>iPhone 13 OLED Soft</span>","₡70,000"], ["<span style='color:#eab308'>iPhone 13 Pro ORIGINAL</span>","₡135,000"], ["<span style='color:#eab308'>iPhone 14 Pro ORIGINAL</span>","₡125,000"], ["<span style='color:#eab308'>iPhone 15 Pro Max ORIGINAL</span>","₡145,000"],
+    ["<span style='color:#eab308'>iPhone X / XS OLED</span>","₡45,000"], ["<span style='color:#eab308'>iPhone XS Max OLED</span>","₡52,000"], ["<span style='color:#eab308'>iPhone 11 ORIGINAL</span>","₡48,000"], ["<span style='color:#eab308'>iPhone 11 Pro Max OLED</span>","₡55,000"], ["<span style='color:#eab308'>iPhone 12 / 12 Pro OLED</span>","₡60,000"], ["<span style='color:#eab308'>iPhone 13 OLED Soft</span>","₡70,000"], ["<span style='color:#eab308'>iPhone 13 Pro ORIGINAL</span>","₡135,000"], ["<span style='color:#eab308'>iPhone 14 Pro ORIGINAL</span>","₡125,000"], ["<span style='color:#eab308'>iPhone 15 Pro Max ORIGINAL</span>","₡145,000"],
 
-    ["TITULO","--- HONOR / HUAWEI (INSTALADA) ---"],
-    ["Honor X6 / X7 / X8","₡28,000"], ["Honor 50 Lite","₡32,000"], ["<span style='color:#eab308'>Honor X9A ORIGINAL</span>","₡55,000"], ["Honor 90 Lite","₡30,000"], ["Huawei Y9 Prime 2019","₡30,000"], ["<span style='color:#eab308'>Huawei Nova 9 SOFT OLED</span>","₡55,000"]
+    ["TITULO","--- HONOR (PANTALLA INSTALADA) ---"],
+    ["Honor X6 / X6S / X6 Pro","₡28,000"], ["Honor X7 / X7A","₡28,000"], ["Honor X7B","₡32,000"], ["Honor X8 / X8A","₡28,000"], ["Honor X8B","₡35,000"], ["Honor X9 / X9 5G","₡30,000"], ["<span style='color:#eab308'>Honor X9A ORIGINAL</span>","₡55,000"], ["Honor 50 Lite","₡32,000"], ["Honor 70 Lite","₡30,000"], ["Honor 90 Lite","₡30,000"], ["Honor Magic 5 Lite","₡55,000"],
+
+    ["TITULO","--- HUAWEI (PANTALLA INSTALADA) ---"],
+    ["Huawei Y5 / Y6 (2019)","₡25,000"], ["Huawei Y7 (2019)","₡28,000"], ["Huawei Y9 (2019)","₡28,000"], ["Huawei Y9 Prime / Y9S","₡30,000"], ["Huawei P30 Lite","₡28,000"], ["Huawei P40 Lite","₡32,000"], ["Huawei Nova 5T","₡32,000"], ["Huawei Nova 8i / 9 SE","₡35,000"], ["<span style='color:#eab308'>Huawei Nova 9 SOFT OLED</span>","₡55,000"]
 ];
 
 const FLEX = [];
