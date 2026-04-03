81510bde899cd3ec78e347802f214839a8d46f45
resettalleraz-max <resettalleraz@gmail.com>
2026-04-03 11:20:50 -0600

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,23 +1,23 @@
 const datos = [
 
-// ==============================================
-// HUAWEI
-// ==============================================
-{ modelos: "P SMART", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 33000 },
-{ modelos: "P SMART + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 39000 },
-{ modelos: "P SMART 2019", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 35000 },
-{ modelos: "P SMART 2019 + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 41000 },
-{ modelos: "P8", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 33000 },
-{ modelos: "P8 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 26000 },
-{ modelos: "P10", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 35000 },
-{ modelos: "P10 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 35000 },
-{ modelos: "P20", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 43000 },
-{ modelos: "P20 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 35000 },
-{ modelos: "P20 LITE + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 41000 },
-{ modelos: "P30 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 41000 },
-{ modelos: "P30 LITE + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 47000 },
-{ modelos: "P40 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 41000 },
-{ modelos: "P40 LITE + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 47000 },
-{ modelos: "Y3 2017", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 33000 }
+    // ==============================================
+    // HUAWEI
+    // ==============================================
+    { modelos: "P SMART", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 33000 },
+    { modelos: "P SMART + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 39000 },
+    { modelos: "P SMART 2019", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 35000 },
+    { modelos: "P SMART 2019 + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 41000 },
+    { modelos: "P8", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 33000 },
+    { modelos: "P8 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 26000 },
+    { modelos: "P10", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 35000 },
+    { modelos: "P10 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 35000 },
+    { modelos: "P20", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 43000 },
+    { modelos: "P20 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 35000 },
+    { modelos: "P20 LITE + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 41000 },
+    { modelos: "P30 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 41000 },
+    { modelos: "P30 LITE + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 47000 },
+    { modelos: "P40 LITE", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 41000 },
+    { modelos: "P40 LITE + MARCO", categoria: "HUAWEI", subcategoria: "CON MARCO", precios: 47000 },
+    { modelos: "Y3 2017", categoria: "HUAWEI", subcategoria: "SIN MARCO", precios: 33000 }
 
 ];
