aa08c2c0c9ef512b44136db5d5fb3f4a8217f8a1
resettalleraz-max <resettalleraz@gmail.com>
2026-04-03 00:21:47 -0600

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,1 +1,23 @@
-
+const productos = [
+    {
+        id: 1,
+        nombre: "Pantalla iPhone 11",
+        descripcion: "Calidad Incell, incluye instalación.",
+        precio: "45,000",
+        stock: true
+    },
+    {
+        id: 2,
+        nombre: "Perfume Nautica Voyage (100ml)",
+        descripcion: "Fragancia fresca para caballero.",
+        precio: "18,500",
+        stock: true
+    },
+    {
+        id: 3,
+        nombre: "Batería Samsung S20",
+        descripcion: "Repuesto original con garantía.",
+        precio: "22,000",
+        stock: false
+    }
+];
